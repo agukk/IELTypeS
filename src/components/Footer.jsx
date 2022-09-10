a31f8b911b94fe92dded052d8663e37646dd12b9
keyyke <space.yokota@gmail.com>
2022-09-10 10:00:27 +0200

    Revert "add typing source"
    This reverts commit a9b1b315088c83025fa459fdef0827197cb87e54.

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Footer = () => {
-    return(
-        <div className='absolute bg-gray-700 w-full bottom-0 flex justify-center py-4 pl-4'>
-            <footer className='text-white font-normal'>© Native Typing - 2022-</footer>
-        </div>
-    );
-}
