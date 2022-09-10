ca4baf6799b7f99c7a63ab75b91fd94f42104815
Yuichiro Luke Smith <contact@yu-smith.com>
2022-09-10 15:31:29 +0900

    fix: app naming

--- ielts.py
+++ ielts.py
@@ -4,23 +4,23 @@ from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import login_required
 
-team = Flask(__name__)
+app = Flask(__name__)
 
-team.config["TEMPLATES_AUTO_RELOAD"] = True
-team.config["SESSION_PERMANENT"] = False
-team.config["SESSION_TYPE"] = "filesystem"
-Session(team)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # db = SQL("sqlite:///team.db")
 
-@ielts.after_request
+@app.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"  # セキュリティ系
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
 
-@ielts.route("/practice", methods=["GET"])
+@app.route("/practice", methods=["GET"])
 # @login_required
 def practice():
     return render_template("practice.html")
