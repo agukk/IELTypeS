ba58a97e4891c12cdbb5bb5bb706300dc12aaab4
agukk <a8120056@gsuite.si.aoyama.ac.jp>
2022-09-20 15:49:30 +0900

    improve typing functions

--- static/js/word_script.js
+++ static/js/word_script.js
@@ -1,11 +1,19 @@
 const topicText = document.querySelector(".topic p");
 const typingText = document.querySelector(".typing-text p");
+let typing = document.querySelector(".wrapper");
 let inpField = document.querySelector(".wrapper .input-field");
 let correctTag = document.querySelector(".correct-types span");
 let mistakeTag = document.querySelector(".miss-types span");
 let accuracyTag = document.querySelector(".accuracy span");
 let timeTag = document.querySelector(".time span b");
 let tryAgainBtn = document.querySelector("button");
+let modal = document.querySelector(".modal-wrapper");
+let modalScore = document.querySelector(".typing-score span");
+let modalCorrect = document.querySelector(".modal-ct span");
+let modalMiss = document.querySelector(".modal-mt span");
+let modalAccuracy = document.querySelector(".modal-accuracy span");
+let againBtn = document.querySelector(".againBtn");
+let homeBtn = document.querySelector(".homeBtn");
 
 let timer;
 let maxTime = 5;
@@ -34,6 +42,7 @@ function randomParagraph() {
     document.addEventListener("keydown", () => inpField.focus());
     typingText.addEventListener("click", () => inpField.focus());
 }
+
 
 function initTyping() {
     const characters = typingText.querySelectorAll("span");
@@ -79,12 +88,13 @@ function initTyping() {
     characters.forEach((char) => char.classList.remove("active"));
     characters[charIndex].classList.add("active");
 
-    const accuracy = (correctTypes / (correctTypes + mistakes) * 100).toFixed(1)
+    const accuracy = (correctTypes / (correctTypes + mistakes) * 100).toFixed(1);
 
     correctTag.innerHTML = correctTypes;
     mistakeTag.innerHTML = mistakes;
     accuracyTag.innerHTML = accuracy;
 }
+
 
 function initTimer() {
     // if timeLeft is greater than 0 then decrement the timeLeft else clear the timer
@@ -95,9 +105,19 @@ function initTimer() {
     }
     else
     {
+        // Timer stop
         clearInterval(timer);
+        // Disable the typing screen and display the modal screen
+        typing.style.display = 'none';
+        modal.style.display = 'inline';
+        // Reflect details of typing results in the modal
+        modalScore.innerHTML = ((correctTypes - mistakes) * (correctTypes / (correctTypes + mistakes)).toFixed(2)).toFixed(0);
+        modalCorrect.innerHTML = correctTypes;
+        modalMiss.innerHTML = mistakes;
+        modalAccuracy.innerHTML = (correctTypes / (correctTypes + mistakes) * 100).toFixed(1);
     }
 }
+
 
 function resetTyping() {
     // calling randomParagraph function and reseting each variables and elements value to default
@@ -111,7 +131,33 @@ function resetTyping() {
     accuracyTag.innerHTML = "";
 }
 
+
+function againTyping() {
+    // calling randomParagraph function and reseting each variables and elements value to default
+    randomParagraph();
+    inpField.value = "";
+    timeLeft = maxTime;
+    charIndex = correctTypes = mistakes = 0;
+    timeTag.innerHTML = timeLeft;
+    correctTag.innerHTML = 0;
+    mistakeTag.innerHTML = 0;
+    accuracyTag.innerHTML = "";
+    isTyping = false;
+    // Disable the modal screen and display the typing screen
+    typing.style.display = 'inline';
+    modal.style.display = 'none';
+}
+
+
+function moveHome() {
+    window.location.href = '/practice';
+}
+
+
 randomParagraph();
 
 inpField.addEventListener("input", initTyping);
 tryAgainBtn.addEventListener("click", resetTyping);
+// Button events for Modal
+homeBtn.addEventListener("click", moveHome);
+againBtn.addEventListener("click", againTyping);
