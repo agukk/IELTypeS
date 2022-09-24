ad934088fe1e81912dedae5353b50a02c69afee2
agukk <a8120056@gsuite.si.aoyama.ac.jp>
2022-09-24 11:14:15 +0900

    cssの修正・sample essayの追加

--- static/js/essay_topic.js
+++ static/js/essay_topic.js
@@ -1,5 +1,5 @@
 const topic = [
-    'topic1',
+    'In recent times, many people are making the decision to live alone. What are the causes of this? Does it have positive or negative effects on society?',
     'topic2',
     'topic3'
 ];
